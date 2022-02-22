9c399391a676d01ee584ace8bb42e05ac6be7017
Ankit Nayan <ankitnayan83@gmail.com>
2022-02-22 11:43:05 +0530

    jai ganesh

--- app.js
+++ app.js
@@ -112,6 +112,7 @@ app.use((err, req, res, next) => {
 })
 
 //starting server
-app.listen(3000, () => {
-    console.log("on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`on port ${port}`);
 })
